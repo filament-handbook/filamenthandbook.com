37e0e3cab43794e75b5b2e1a708b707d1760f072
Andréia Bohner <andreiabohner@gmail.com>
2024-10-20 21:47:52 -0300

    Initial commit

--- src/scripts/alpine.js
+++ src/scripts/alpine.js
@@ -1,5 +1,10 @@
 import Alpine from "./module.esm.js";
-
+import intersect from './intersect.esm.js';
+import focus from './focus.esm.js';
+ 
 window.Alpine = Alpine;
+
+Alpine.plugin(intersect);
+Alpine.plugin(focus);
 
 Alpine.start();
